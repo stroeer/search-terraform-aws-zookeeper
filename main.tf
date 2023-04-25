ba943128239bbeaf086588e6296c41310b94685e
Michael Gaber <github@fennnec.de>
2023-04-25 09:11:23 +0200

    add internal communication egress rules

--- main.tf
+++ main.tf
@@ -262,6 +262,24 @@ resource "aws_vpc_security_group_ingress_rule" "zookeeper-bastion" {
   ip_protocol = "tcp"
 }
 
+resource "aws_vpc_security_group_egress_rule" "zookeeper_internal_egress_2888" {
+  security_group_id            = aws_security_group.zookeeper-internal.id
+  referenced_security_group_id = aws_security_group.zookeeper-internal.id
+
+  from_port   = 2888
+  to_port     = 2888
+  ip_protocol = "tcp"
+}
+
+resource "aws_vpc_security_group_egress_rule" "zookeeper_external_egress_3888" {
+  security_group_id            = aws_security_group.zookeeper-external.id
+  referenced_security_group_id = aws_security_group.zookeeper-internal.id
+
+  from_port   = 3888
+  to_port     = 3888
+  ip_protocol = "tcp"
+}
+
 resource "aws_vpc_security_group_egress_rule" "zookeeper_external_egress_https_v4" {
   security_group_id = aws_security_group.zookeeper-external.id
 
@@ -274,7 +292,7 @@ resource "aws_vpc_security_group_egress_rule" "zookeeper_external_egress_https_v
 resource "aws_vpc_security_group_egress_rule" "zookeeper_external_egress_https_v6" {
   security_group_id = aws_security_group.zookeeper-external.id
 
-  cidr_ipv6   = "::0/0"
+  cidr_ipv6   = "::/0"
   from_port   = 443
   to_port     = 443
   ip_protocol = "tcp"
